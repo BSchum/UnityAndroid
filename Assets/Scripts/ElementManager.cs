d915992cbc1118ad6962e36ff2d4d266ca39d6f0
brice schumacher <brice.schumacher@ynov.com>
2020-03-19 12:07:21 +0100

    Zoom on element

---
+++ Assets/Scripts/ElementManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class ElementManager : MonoBehaviour
+{
+    [SerializeField] List<GameObject> elements;
+    [SerializeField] Transform zoomedTransform;
+
+    GameObject _currentElement = null; 
+    Vector3 _currentElementLastPosition;
+
+    private void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit raycastHit;
+
+            if (Physics.Raycast(raycast, out raycastHit))
+            {
+                OnElementTouched(raycastHit.collider.gameObject);
+            }
+        }
+    }
+
+    private void OnElementTouched(GameObject element)
+    {
+        Debug.Log($"{element.name} is touched!");
+        if(_currentElement != null)
+            _currentElement.transform.position = _currentElementLastPosition;
+
+        _currentElement = element;
+        _currentElementLastPosition = element.transform.position;
+        element.transform.position = zoomedTransform.position;
+    }
+}
+
